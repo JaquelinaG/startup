0492bc05438a19c0657811482cab2bfce6f0d555
Jaquelina <jaquelinagiuliani@gmail.com>
2018-09-09 20:53:03 -0300

    Avance DOM

--- 01-JS-DOM-APIs/animation.js
+++ 01-JS-DOM-APIs/animation.js
@@ -6,13 +6,55 @@ const fadeIn= elem =>{
 
 function showAlert(){
     alert('You clicked me!');
-} 
+}
 
-function createTable(data){
-    const array = [
-        ['jaquelina', 36547892],
-        ['romina', 25479634],
-        ['melina', 31478965]
-    ]
+function createTable(array){
+    let table = createNode('table');
+    let tableBody = createNode('tbody');
+    let tableHead = createNode('thead');
 
+    for(let i = 0; i < array.length; i++ ){
+        let persona = array[i];
+        let tr = createNode('tr');
+
+        for(let j = 0; j < persona.length; j++){            
+            let td = createNode('td');
+            let value = document.createTextNode(persona[j]);
+            td.appendChild(value);
+            tr.appendChild(td);            
+        }
+
+        tableBody.appendChild(tr);        
+
+        var columns = persona.length;
+    }
+
+    let trHead = createNode('tr');
+    for(let c = 0; c < columns.length; c ++){
+        let th = document.createNode('th');
+        let value = createTextNode('Column');
+        th.appendChild(value);
+        trHead.appendChild(th);        
+    }
+    tableHead.appendChild(trHead);        
+    
+    table.appendChild(tableHead);
+    table.appendChild(tableBody);
+    
+    return table;
+
+    // let items = data.items;
+    //     return items.map(function(item){
+    //         let li = createNode('li');
+    //         let span = createNode('span');
+
+    //         span.innerHTML = item.full_name;
+    //         li.appendChild(span);
+    //         ul.appendChild(li);
+    //     })
+
+}
+
+function createNode(element){
+    return document.createElement(element);
 }
